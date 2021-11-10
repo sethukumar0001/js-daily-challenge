390a03f48491cff7ebdf7cd0d1da3076f85e526e
sethukumar0001 <boylooksrock@gmail.com>
2021-11-10 23:58:43 +0530

    updated merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,6 +1,25 @@
 
-let arr = [5, 3, 23, 10, 1, 9, 8];
+let arr = [5, 3, 23, 10, 1, 9, 8, 33, 2, 43];
 function mergeSort(arr) {
-    return arr;
+    let mid = Math.floor(arr.length / 2);
+    if (arr.length < 2) {
+        return arr;
+    }
+    let left = arr.splice(0, mid);
+    // console.log(left,"======>left")
+    // console.log(arr,"------->arr")
+    return merge(mergeSort(left), mergeSort(arr))
+}
+
+const merge = (left, right) => {
+    let arr = []
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift())
+        } else {
+            arr.push(right.shift())
+        }
+    }
+    return [...arr, ...left, ...right]
 }
 console.log(mergeSort(arr))
---
+++ quickSort.js
@@ -0,0 +1,8 @@
+
+
+let arr = [5, 3, 23, 10, 1, 9, 8];
+function quickSort(arr) {
+
+    return arr;
+}
+console.log(quickSort(arr))
