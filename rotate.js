e7c8d95575f7b7bf9706a6f980d4490f7f3531ba
sethukumar0001 <boylooksrock@gmail.com>
2021-11-23 23:00:19 +0530

    updated code

--- rotate.js
+++ rotate.js
@@ -1,10 +1,9 @@
 function rotate(arr, n) {
     var x = arr[n - 1], i;
-    for (i = n - 1; i > 0; i--) {
+    for (i = n - 1; i > 0; i--)
         arr[i] = arr[i - 1];
-        arr[0] = x;
-        return arr
-    }
+    arr[0] = x;
+    return arr
 }
 var arr = [1, 2, 3, 4, 5];
 var n = arr.length;
