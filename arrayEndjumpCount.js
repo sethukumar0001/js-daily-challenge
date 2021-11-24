b859f25066a9a0c46dff058f43b518435548f648
sethukumar0001 <boylooksrock@gmail.com>
2021-11-25 00:11:27 +0530

    Minimum number of jumps to reach end

---
+++ arrayEndjumpCount.js
@@ -0,0 +1,26 @@
+function arrayEndJump(arr) {
+
+    let ladder = arr[0];
+    let stirs = arr[0];
+    let jump = 1;
+
+    if (arr.length <= 1) {
+        return 0;
+    }
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (i + arr[i] > ladder)
+            ladder = i + arr[i]
+        stirs--;
+        if (stirs === 0) {
+            jump++;
+            stirs = ladder - i
+        }
+
+    }
+    return jump
+
+}
+var arr = [1, 4, 3, 7, 1, 2, 6, 7, 6, 10];
+
+console.log(arrayEndJump(arr))
